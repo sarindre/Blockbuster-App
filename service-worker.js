b1831e403d05987221d03dd4f3b5ad544a048127
sarindre <treywleonard@gmail.com>
2026-04-08 20:41:39 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 
-const CACHE = 'family-movie-vault-v1';
+const CACHE = 'family-movie-vault-v2';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener('fetch', e => {
